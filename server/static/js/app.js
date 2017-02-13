5603e859f191ba8105cf2716a45b7aea165cff53
Håkan MacLean <hakan@maclean.se>
2017-02-13 14:33:39 +0100

    Removed hard coded value in GET request

--- server/static/js/app.js
+++ server/static/js/app.js
@@ -1,4 +1,4 @@
-'use strict';   // See note about 'use strict'; below
+'use strict'; //tells to compiler to not accept 'implicit' variables
 
 var myApp = angular.module('myApp', [
  'ngRoute'
@@ -19,18 +19,19 @@ myApp.controller('booliUrlController', function($rootScope, $scope) {
         $scope.booliUrl = "https://www.booli.se/karlaplan/149545/";
         $scope.workAddress = "Sveavägen 25, Stockholm";
         $scope.getListings = function() {
+            console.log($scope.booliUrl);
             $rootScope.$emit('newBooliUrl', $scope.booliUrl, $scope.workAddress);
         }
     });
 
 myApp.controller('listingsController', function($rootScope, $scope, $http) {
     $rootScope.$on('newBooliUrl', function(event, url, address) {
-        $scope.address = address;
+        $scope.destinationAddress = address;
         loadListings(url);
     });
 
     var loadListings = function(url) {
-        $http.get('http://localhost:5000/api/listings?https://www.booli.se/karlaplan/149545/').
+        $http.get('http://localhost:5000/api/listings?'+url).
         then(function(response) {
             $scope.listings = response.data["listings"];
         });
@@ -39,7 +40,7 @@ myApp.controller('listingsController', function($rootScope, $scope, $http) {
 
 myApp.controller('durationController', function($scope, $http) {
     var origin = $scope.listing.location.position.latitude + "," + $scope.listing.location.position.longitude;
-    var url = '/api/duration?origin='+origin+'&destination='+$scope.address
+    var url = '/api/duration?origin='+origin+'&destination='+$scope.destinationAddress
     $http.get(url).
         then(function(response) {
             $scope.listing.duration = response.data["duration"];
