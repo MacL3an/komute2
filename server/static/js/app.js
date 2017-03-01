e255070ce8fb815d7521842c668d40f9c6f8a8dc
Håkan MacLean <hakan@maclean.se>
2017-03-01 11:21:47 +0100

    First versioin with static data

--- server/static/js/app.js
+++ server/static/js/app.js
@@ -1,68 +1,155 @@
 'use strict'; //tells to compiler to not accept 'implicit' variables
 
-var myApp = angular.module('myApp', [
- 'ngRoute'
-]);
-
-myApp.config(['$routeProvider',
-     function($routeProvider) {
-         $routeProvider.
-             when('/', {
-                 templateUrl: '/static/partials/listings.html',
-             }).
-             otherwise({
-                 redirectTo: '/'
-             });
-    }]);
-
-myApp.controller('booliUrlController', function($rootScope, $scope) {
-        $scope.booliUrl = "https://www.booli.se/soderort/914052/?objectType=Parhus%2CRadhus%2CKedjehus&rooms=4";
-        $scope.workAddress = "Sveavägen 25, Stockholm";
-        $scope.transitTypes = [{
-                Id: 'transit',
-                Name: 'Public Transport'
-            }, {
-                Id: 'bicycle',
-                Name: 'Bicycle'
-            }, {
-                Id: 'walking',
-                Name: 'Walking'
-            }, {
-                Id: 'driving',
-                Name: 'Driving'
-            }];
-        $scope.transitType = $scope.transitTypes[0].Id;
-        $scope.getListings = function() {
-            $rootScope.$emit('getListings', $scope.booliUrl, $scope.workAddress, $scope.transitType);
-        }
-    });
-
-myApp.controller('listingsController', function($rootScope, $scope, $http) {
-    $rootScope.$on('getListings', function(event, url, address, transitType) {
-        $scope.destinationAddress = address;
-        $scope.transitType = transitType;
-        loadListings(url);
-    });
-
-    var loadListings = function(url) {
-        $http.get('/api/listings?'+url).
-        then(function(response) {
-            $scope.listings = response.data["listings"];
-        });
+class ListingRow extends React.Component {
+    render() {
+        var address = this.props.listing.location.address.streetAddress;
+        var rooms = this.props.listing.rooms;
+        var size = this.props.listing.livingArea;
+        var price = this.props.listing.listPrice;
+        var duration = -1;
+        return (
+            <tr>
+                <td>{address}</td>
+                <td>{rooms}</td>
+                <td>{size}</td>
+                <td>{price}</td>
+                <td>{duration}</td>
+            </tr>
+        );
     }
-});
+}
 
-myApp.controller('durationController', function($scope, $http) {
-    var origin = $scope.listing.location.position.latitude + "," + $scope.listing.location.position.longitude;
-    var url = '/api/duration?origin='+origin+'&destination='+$scope.destinationAddress+'&transitType='+$scope.transitType
-    $http.get(url).
-        then(function(response) {
-            $scope.listing.duration = response.data["duration"];
+class ListingsTable extends React.Component {
+    render() {
+    var rows = [];
+
+    this.props.listings.forEach(function(listing) {
+        rows.push(<ListingRow listing={listing} key={listing.booliId} />);
     });
-});
 
-myApp.filter('secondsToDateTime', function() {
-    return function(seconds) {
-        return new Date(0, 0, 0).setSeconds(seconds);
-    };
-});
+    return (
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Address</th>
+            <th>Rooms</th>
+            <th>Size</th>
+            <th>Price</th>
+            <th>Duration</th>
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </table>
+    );}
+}
+
+var LISTINGS = [
+            {
+              "booliId": 2253361,
+              "constructionYear": 1929,
+              "floor": 3,
+              "listPrice": 3475000,
+              "livingArea": 52,
+              "location": {
+                "address": {
+                  "streetAddress": "Thorildsv\u00e4gen 5"
+                },
+                "namedAreas": [
+                  "Kungsholmen"
+                ],
+                "position": {
+                  "latitude": 59.33227009,
+                  "longitude": 18.01635504
+                },
+                "region": {
+                  "countyName": "Stockholms l\u00e4n",
+                  "municipalityName": "Stockholm"
+                }
+              },
+              "objectType": "L\u00e4genhet",
+              "plotArea": 0,
+              "published": "2017-02-03 23:52:12",
+              "rent": 3039,
+              "rooms": 2,
+              "source": {
+                "id": 1564,
+                "name": "Erik Olsson Fastighetsf\u00f6rmedling",
+                "type": "Broker",
+                "url": "http://www.erikolsson.se/"
+              },
+              "url": "https://www.booli.se/annons/2253361"
+            },
+            {
+              "booliId": 2177621,
+              "constructionYear": 2016,
+              "floor": 5,
+              "listPrice": 3975000,
+              "livingArea": 53,
+              "location": {
+                "address": {
+                  "streetAddress": "Hornsbergs Strand 15C"
+                },
+                "namedAreas": [
+                  "Kungsholmen"
+                ],
+                "position": {
+                  "latitude": 59.34049523,
+                  "longitude": 18.00779643
+                },
+                "region": {
+                  "countyName": "Stockholms l\u00e4n",
+                  "municipalityName": "Stockholm"
+                }
+              },
+              "objectType": "L\u00e4genhet",
+              "published": "2017-02-03 22:11:11",
+              "rent": 2871,
+              "rooms": 2,
+              "source": {
+                "id": 410,
+                "name": "Edward & Partners Fastighetsm\u00e4klare AB",
+                "type": "Broker",
+                "url": "http://www.edwardpartners.se/"
+              },
+              "url": "https://www.booli.se/annons/2177621"
+            },
+            {
+              "booliId": 1234,
+              "constructionYear": 2016,
+              "floor": 5,
+              "listPrice": 5000000,
+              "livingArea": 100,
+              "location": {
+                "address": {
+                  "streetAddress": "Testgata 1"
+                },
+                "namedAreas": [
+                  "Kungsholmen"
+                ],
+                "position": {
+                  "latitude": 59.34049523,
+                  "longitude": 18.00779643
+                },
+                "region": {
+                  "countyName": "Stockholms l\u00e4n",
+                  "municipalityName": "Stockholm"
+                }
+              },
+              "objectType": "L\u00e4genhet",
+              "published": "2017-02-03 22:11:11",
+              "rent": 2871,
+              "rooms": 3,
+              "source": {
+                "id": 410,
+                "name": "Edward & Partners Fastighetsm\u00e4klare AB",
+                "type": "Broker",
+                "url": "http://www.edwardpartners.se/"
+              },
+              "url": "https://www.booli.se/annons/2177621"
+            }
+          ];
+
+ReactDOM.render(
+    <ListingsTable listings={LISTINGS}/>,
+    document.getElementById('root')
+);
