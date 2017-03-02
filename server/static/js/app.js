213e9d69dd1f0ad4bc066e2e5b02b71388f1b6c5
Håkan MacLean <hakan@maclean.se>
2017-03-02 13:21:35 +0100

    Converted to ES2015 classes, added static forms

--- server/static/js/app.js
+++ server/static/js/app.js
@@ -1,32 +1,32 @@
 'use strict'; //tells to compiler to not accept 'implicit' variables
 
-var DurationCell = React.createClass({
-    getInitialState: function() {
-        return {
+class DurationCell extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
             duration: ''
         };
-    },
+    }
 
-    componentDidMount: function() {
+    componentDidMount() {
         var url = '/api/duration?origin='+this.props.origin+'&destination='+this.props.destination+'&transitType='+this.props.transitType
-        console.log(url)
         var that = this;
         this.serverRequest = $.get(url, function(result) {
             that.setState({
                 duration: result.duration
             });
         });
-    },
+    }
 
-    render: function() {
+    render() {
         return (
             <td>{this.state.duration}</td>
         );
     }
-});
+}
 
-var ListingRow = React.createClass({
-    render: function() {
+class ListingRow extends React.Component{
+    render() {
         var address = this.props.listing.location.address.streetAddress;
         var rooms = this.props.listing.rooms;
         var size = this.props.listing.livingArea;
@@ -42,25 +42,26 @@ var ListingRow = React.createClass({
             </tr>
         );
     }
-});
+}
 
-var ListingsTable = React.createClass({
-    getInitialState: function() {
-        return {
+class ListingsTable extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
             listings: []
         };
-    },
+    }
 
-    componentDidMount: function() {
+    componentDidMount() {
         var apiAddress = "api/listings?https://www.booli.se/soderort/914052/?objectType=Parhus%2CRadhus%2CKedjehus&rooms=4";
         this.serverRequest = $.get(apiAddress, function(result) {
             this.setState({
                 listings: result.listings
             });
         }.bind(this));
-    },
+    }
 
-    render: function() {
+    render() {
             var rows = [];
             this.state.listings.forEach(function(listing) {
                 rows.push(<ListingRow listing={listing} key={listing.booliId} />);
@@ -79,10 +80,42 @@ var ListingsTable = React.createClass({
             </thead>
             <tbody>{rows}</tbody>
           </table>
-        );}
-    });
+    );
+    }
+}
+
+class KomuterApp extends React.Component{
+    render() {
+        return (
+            <div>
+                <form>
+                    <div>
+                        <div className="form-group">
+                            <label>Booli URL: </label>
+                            <input id="booliUrl" className="form-control" type="text"/>
+                        </div>
+                        <div className="form-group">
+                            <label>Work address: </label>
+                            <input type="text" className="form-control"/>
+                        </div>
+                        <div className="form-group btn-space">
+                            <label>Transit type: </label><br/>
+                            <select>
+                            </select>
+                            </div>
+                        <div className="form-group">
+                            <input className="btn btn-primary btn-space" type="button" value="Show listings"/>
+                        </div>
+                    </div>
+                </form>
+                <h3>Listings:</h3>
+                <ListingsTable/>
+            </div>
+        );
+    }
+}
 
 ReactDOM.render(
-    <ListingsTable/>,
+    <KomuterApp/>,
     document.getElementById('root')
 );
